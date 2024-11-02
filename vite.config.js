1f5a6590aa394ce2911fd0eea56be55ffb0d47bc
BobchopGaming <jmomolon@gmail.com>
2024-11-02 19:02:59 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
+    // Remove the external options to ensure dependencies are bundled
     rollupOptions: {
-      external: ['react-router-dom', 'emailjs-com'], // Add emailjs-com as an external dependency
+      // No external dependencies should be specified here
+      // external: ['react-router-dom', 'emailjs-com'], 
     },
   },
 });
